359bd9a6fb9868708da01a9c37b87da7cc3acb2a
Raimondas Rimkevičius <github@mekdrop.name>
2021-02-25 03:20:08 +0200

    Added compatibility when accessing variable values directly

--- src/CriteriaElement.php
+++ src/CriteriaElement.php
@@ -24,4 +24,90 @@ abstract class CriteriaElement
      * @return array
      */
     abstract public function getBindData(): array;
+
+    /**
+     * For compatibility. Probably will be removed in feature
+     * Gets variable value
+     *
+     * @param string $name Variable name
+     *
+     * @return mixed
+     */
+    public function __get($name)
+    {
+        switch ($name) {
+            case 'order':
+                return (string)$this->getOrder();
+            case 'sort':
+                return $this->getSort();
+            case 'limit':
+                return $this->getLimit();
+            case 'start':
+                return $this->getStart();
+            case 'groupBy':
+                /** @noinspection SpellCheckingInspection */
+            case 'groupby':
+                return $this->getGroupBy();
+        }
+    }
+
+    /**
+     * For compatibility. Probably will be removed in feature
+     * Sets variable
+     *
+     * @param string $name Variable name
+     * @param mixed $value Variable value
+     *
+     * @return mixed
+     */
+    public function __set($name, $value)
+    {
+        switch ($name) {
+            case 'order':
+                $this->setOrder($value);
+                break;
+            case 'sort':
+                $this->setSort($value);
+                break;
+            case 'limit':
+                $this->setLimit($value);
+                break;
+            case 'start':
+                $this->setStart($value);
+                break;
+            case 'groupBy':
+                /** @noinspection SpellCheckingInspection */
+            case 'groupby':
+                $this->setGroupBy($value);
+                break;
+        }
+    }
+
+    /**
+     * For compatibility. Probably will be removed in feature
+     * Checks if variable value is set
+     *
+     * @param string $name Variable name
+     *
+     * @return mixed
+     */
+    public function __isset($name)
+    {
+        switch ($name) {
+            case 'order':
+                return $this->getOrder() !== null;
+            case 'sort':
+                return !empty($this->getSort());
+            case 'limit':
+                return !empty($this->getLimit());
+            case 'start':
+                return !empty($this->getStart());
+            case 'groupBy':
+                /** @noinspection SpellCheckingInspection */
+            case 'groupby':
+                return !empty($this->getGroupBy());
+            default:
+                return false;
+        }
+    }
 }
